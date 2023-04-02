8c9cb40a9afb2d6358bd3931cc06d856980c903b
AmdjedBelgacem <amgedbelgacem@gmail.com>
2023-04-02 04:10:56 +0300

    Done

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
 // Write your code here
+let num1 = 31; 
+let num2 = 2;
+let num3 = 16; 
+let num4 = 4;  
+let multiply = num1*num2;  
+let random = Math.ceil(Math.random());
+let mod = num3/num4;
+let max = Math.max(1,3,5,9,7,5,1,3,20);
